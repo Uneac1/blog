8364da17c6603ac514c467b4befae8375290bc80
Uneac1 <1006849568@qq.com>
2025-05-28 13:17:01 +0800

    Site updated: 2025-05-28 13:17:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/27/Pages/","2025/05/27/hello-world/","2025/05/27/测试/"];function toRandomPost(){
+var posts=["2025/05/27/hello-world/","2025/05/27/测试/","2025/05/27/Pages/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
