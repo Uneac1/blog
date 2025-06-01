7159aabf80f1af2e7aba8f386a216e9b344225ae
Uneac1 <1006849568@qq.com>
2025-06-01 17:55:41 +0800

    Site updated: 2025-06-01 17:55:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/27/Pages/","2025/05/27/Test/","2025/05/27/hello-world/","2025/05/29/hexo/","2025/05/28/tt/","2025/05/31/PaleBlueDot/","2025/06/01/life/"];function toRandomPost(){
+var posts=["2025/05/27/Pages/","2025/05/31/PaleBlueDot/","2025/05/27/Test/","2025/05/27/hello-world/","2025/05/29/hexo/","2025/06/01/life/","2025/05/28/tt/","2025/06/01/weight/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
